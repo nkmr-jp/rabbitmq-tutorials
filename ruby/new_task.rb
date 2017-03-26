f7d95b8459e298f51175ad60f276d8c278958aeb
nkmr-jp <nkmr.git@gmail.com>
2017-03-26 19:19:25 +0900

    [mod] 2 worker queue スタイル変更

--- ruby/new_task.rb
+++ ruby/new_task.rb
@@ -1,17 +1,14 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
+require 'bunny'
 
-require "bunny"
-
-conn = Bunny.new(:automatically_recover => false)
+conn = Bunny.new(automatically_recover: false)
 conn.start
 
-ch   = conn.create_channel
-q    = ch.queue("task_queue", :durable => true)
+ch = conn.create_channel
+q  = ch.queue('task_queue', durable: true)
 
-msg  = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
+msg = ARGV.empty? ? 'Hello World!' : ARGV.join(' ')
 
-q.publish(msg, :persistent => true)
+q.publish(msg, persistent: true)
 puts " [x] Sent #{msg}"
 
 conn.close
