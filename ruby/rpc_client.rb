64f6fd4b74a5979fb422c75b9e3d98bb5d2bccdb
nkmr-jp <nkmr.git@gmail.com>
2017-03-29 08:44:31 +0900

    [add] 6 rpc コメント追加

--- ruby/rpc_client.rb
+++ ruby/rpc_client.rb
@@ -20,13 +20,14 @@ class FibonacciClient
     @x              = ch.default_exchange
 
     @server_queue   = server_queue
-    @reply_queue    = ch.queue("", :exclusive => true)
+    @reply_queue    = ch.queue("", :exclusive => true) #匿名の排他コールバックキュー
 
 
     @lock      = Mutex.new
     @condition = ConditionVariable.new
     that       = self
 
+    # 戻りの一時キューを購読する。
     @reply_queue.subscribe do |delivery_info, properties, payload|
       if properties[:correlation_id] == that.call_id
         that.response = payload.to_i
@@ -36,12 +37,12 @@ class FibonacciClient
   end
 
   def call(n)
-    self.call_id = self.generate_uuid
+    self.call_id = self.generate_uuid #ランダムなIDを発行している
 
     @x.publish(n.to_s,
-      :routing_key    => @server_queue,
-      :correlation_id => call_id,
-      :reply_to       => @reply_queue.name)
+      :routing_key    => @server_queue,      # rpc_queue というキュー名がルーティングキーとなる。
+      :correlation_id => call_id,            #一時的につかうランダムなID
+      :reply_to       => @reply_queue.name)  #これが匿名キュー？テンポラリ？ワーカー(サーバー)はこのキューを使ってジョブが終わったとメッセージをかえす。
 
     lock.synchronize{condition.wait(lock)}
     response
@@ -64,3 +65,17 @@ puts " [.] Got #{response}"
 
 ch.close
 conn.close
+
+# で第二のチュートリアル、私たちは、使用する方法を学びました作業キューを複数の労働者の間で時間のかかるタスクを分散します。
+# しかし、リモートコンピュータ上で関数を実行し、その結果を待つ必要がある場合はどうすればよいでしょうか？まあ、それは別の話です。
+# このパターンは、一般にリモートプロシージャコールまたはRPCと呼ばれます。
+# このチュートリアルでは、RabbitMQを使用してRPCシステムを構築します。
+# クライアントとスケーラブルなRPCサーバーです。
+# 配布に時間を費やす作業がないので、フィボナッチ数を返すダミーRPCサービスを作成します。
+
+# ：persistent：メッセージを永続的（trueの値）または一時的（false）としてマークします。
+#     2番目のチュートリアルからこのプロパティを覚えているかもしれません。
+# ：content_type：エンコーディングのMIMEタイプを記述するために使用されます。
+#     たとえば、頻繁に使用されるJSONエンコーディングの場合、このプロパティをapplication / jsonに設定することをお勧めします。
+# ：reply_to：コールバックキューに名前を付けるためによく使用されます。
+# ：correlation_id：RPC応答を要求と相関させるのに便利です。
